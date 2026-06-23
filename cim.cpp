9b2f458fc08744ff177398b749a990c749566495
Luiz <luizgoud.editor@gmail.com>
2026-06-23 16:44:08 -0300

    Commit

---
+++ cim.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+using namespace std;
+
+typedef vector<vector<int>> Lista;
+
+int n;
+Lista G;
+vector<int> melhor;
+
+// verifica se pode adicionar o vértice
+bool podeAdicionar(int v, vector<int>& atual) {
+    for (int u : atual) {
+        for (int viz : G[v]) {
+            if (viz == u) return false;
+        }
+    }
+    return true;
+}
+
+// backtracking
+void backtrack(int v, vector<int>& atual) {
+    if (v == n) {
+        if (atual.size() > melhor.size()) {
+            melhor = atual;
+        }
+        return;
+    }
+
+    // inclui v se possível
+    if (podeAdicionar(v, atual)) {
+        atual.push_back(v);
+        backtrack(v + 1, atual);
+        atual.pop_back();
+    }
+
+    // não inclui v
+    backtrack(v + 1, atual);
+}
+
+void leituraGrafo(Lista &G, int m) {
+    int a, b;
+    while (m--) {
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+}
+
+void escritaGrafo(Lista &G) {
+    int n = G.size();
+    cout << "Lista de Adjacencia:\n";
+    for (int u = 0; u < n; u++) {
+        cout << u << ": ";
+        for (auto v : G[u]) {
+            cout << v << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    ifstream arquivo("grafo.txt");
+
+    if (!arquivo) {
+        cout << "Nao deu certo abrir" << endl;
+        return 1;
+    }
+
+    int m;
+    arquivo >> n >> m;
+
+    G.assign(n, vector<int>());
+
+    int a, b;
+    while (m--) {
+        arquivo >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    arquivo.close();
+
+    escritaGrafo(G);
+
+    vector<int> atual;
+    backtrack(0, atual);
+
+    cout << "\nConjunto Independente Maximo:\n";
+    for (int v : melhor) {
+        cout << v << " ";
+    }
+
+    cout << "\nTamanho: " << melhor.size() << endl;
+
+    return 0;
+}
